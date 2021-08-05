f059637bbb285efb1a3f64ce7e55b5a6bb172363
nolanworthy <nolan@worthyplace.com>
2021-08-05 16:36:11 -0400

    Initial state of project.

---
+++ Calendar.java
@@ -0,0 +1,8 @@
+//import java-calendar-project.Event.java;
+
+public class Calendar {
+    public static void main(String[] args) {
+        Event myEvent = new Event("this is the name");
+        System.out.println(myEvent.getEventName());
+    }
+}
---
+++ Event.java
@@ -0,0 +1,11 @@
+public class Event {
+    private String eventName;
+
+    public Event(String newName) {
+        eventName = newName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+}
