17fd90adb181a071dee79425d3415683a94e9eb4
nolanworthy <nolan@worthyplace.com>
2021-08-05 18:17:45 -0400

    began adding instance variables and member functions

--- Event.java
+++ Event.java
@@ -1,11 +1,21 @@
+import java.time.*;
+
 public class Event {
     private String eventName;
+    private boolean isAllDay;
+    private LocalDate eventDate;
+    private LocalTime eventTime;
 
-    public Event(String newName) {
-        eventName = newName;
+    public Event(String eventName, int hour, int minute) {
+       this.eventName = eventName;
+       this.eventTime = LocalTime.of(hour, minute);
     }
 
-    public String getEventName() {
-        return eventName;
+    public String getName() {
+         return eventName;
+    }
+
+    public LocalTime getTime() {
+        return eventTime;
     }
 }
