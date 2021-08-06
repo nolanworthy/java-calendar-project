f0d635cda312dcb84e7386cd2558906b087ddb2a
nolanworthy <nolan@worthyplace.com>
2021-08-06 16:04:38 -0400

    expanded Event methods

--- Event.java
+++ Event.java
@@ -6,16 +6,83 @@ public class Event {
     private LocalDate eventDate;
     private LocalTime eventTime;
 
-    public Event(String eventName, int hour, int minute) {
+    //constructor with date as integers and time as integers
+    public Event(String eventName, int year, int month, int dayOfMonth, int hour, int minute) {
        this.eventName = eventName;
+       this.eventDate = LocalDate.of(year, month, dayOfMonth);
        this.eventTime = LocalTime.of(hour, minute);
     }
 
-    public String getName() {
-         return eventName;
+    //constructor with date as LocalDate and time as integers
+    public Event(String eventName, LocalDate eventDate, int hour, int minute) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventTime = LocalTime.of(hour, minute);
     }
 
-    public LocalTime getTime() {
-        return eventTime;
+    //constructor with date as integers and time as LocalTime
+    public Event(String eventName, int year, int month, int dayOfMonth, LocalTime eventTime) {
+        this.eventName = eventName;
+        this.eventDate = LocalDate.of(year, month, dayOfMonth);
+        this.eventTime = eventTime;
     }
+
+    //contructor with date as LocalDate and time as Local Time
+    public Event(String eventName, LocalDate eventDate, LocalTime eventTime) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+    }
+
+    //constructor with date as integers and event being all day
+    public Event(String eventName, int year, int month, int dayOfMonth, boolean isAllDay) {
+        this.eventName = eventName;
+        this.eventDate = LocalDate.of(year, month, dayOfMonth);
+        this.isAllDay = isAllDay;
+    }
+
+    //constructor with date as LocalDate and event being all day
+    public Event(String eventName, LocalDate eventDate, boolean isAllDay) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.isAllDay = isAllDay;
+    }
+
+    public void setName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getName() {
+         return this.eventName;
+    }
+
+    public void setIsAllDay(boolean isAllDay) {
+        this.isAllDay = isAllDay;
+    }
+
+    public boolean getIsAllDay() {
+        return this.isAllDay;
+    }
+
+    //date setter where parameters are integers
+    public void setDate(int year, int month, int dayOfMonth) {
+        this.eventDate = LocalDate.of(year, month, dayOfMonth);
+    }
+
+    //date setter where parameter is a LocalDate object
+    public void setDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public LocalDate getDate() {
+        return this.eventDate;
+    }
+
+    //add time setters
+
+    public LocalTime getTime() {
+        return this.eventTime;
+    }
+
+    //add print method
 }
